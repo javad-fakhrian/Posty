faba987158122747227520a706a3bf73db589f6b
Javad Fakhrian <javadjavadr@gmail.com>
2021-03-10 12:45:31 +0330

    login
    route , controller , use middleware , view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 //use App\Http\Controllers\Auth\RegisterController;
 
@@ -16,12 +18,21 @@ use App\Http\Controllers\Auth\RegisterController;
 |
 */
 
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 Route::get('/register', [RegisterController::class , 'index'])->name('register');
 Route::post('/register', [RegisterController::class , 'store']);
+
+Route::get('/login', [LoginController::class , 'index'])->name('login');
+Route::post('/login', [LoginController::class , 'store']);
+
+Route::get('/logout' , [LogoutController::class , 'store'])->name('logout');
 
 Route::get('/dashboard',[DashboardController::class , 'index'])->name('dashboard');
 
 Route::get('/posts', function () {
     // return view('welcome');
     return view('posts.index');
-});
+})->name('posts');
